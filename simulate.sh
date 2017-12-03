e1b2a1e9db22c5520673a253f3d486ccee6b3c65
Hsieh, Chiao <chsieh16@illinois.edu>
2017-12-03 17:04:56 -0600

    Merge multiple Maven invocations

--- simulate.sh
+++ simulate.sh
@@ -2,7 +2,4 @@
 python simulate.py $1
 an=$(basename "$1" .krd) # FIXME This doesn't work if filename is not lowercase of program name
 
-mvn compile 
-mvn install
-HOME_DIR=`echo "$HOME"`
-mvn exec:java -Dexec.mainClass="testSim.apps.$an.Main"
+mvn compile exec:java -Dexec.mainClass="testSim.apps.$an.Main"

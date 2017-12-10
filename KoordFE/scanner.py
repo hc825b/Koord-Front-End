dbf12268e19564c7de990daebb0a57e7a8e88914
Hsieh, Chiao <chsieh16@illinois.edu>
2017-12-09 21:43:16 -0600

    Remove uncessary string to number conversion
    +/- signs are handled by unary operators

--- KoordFE/scanner.py
+++ KoordFE/scanner.py
@@ -36,17 +36,10 @@ tokens = [
 
 # decimal numbers. Note that this rule is before integer numbers so that this
 # rule is attempted first
-def t_FNUM(t):
-    r'[-]?[0-9]+[.][0-9]+'
-    t.value = float(t.value)
-    return t
-
+t_FNUM = r'\d+\.\d+'
 
 # integer numbers
-def t_INUM(t):
-    r'[-]?[0-9]+'
-    t.value = int(t.value)
-    return t
+t_INUM = r'\d+'
 
 
 # delimiters
